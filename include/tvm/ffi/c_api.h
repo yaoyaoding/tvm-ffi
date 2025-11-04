00a9ad7df61bef0c1af47aef7a8af0a0e68da2f5
Tianqi Chen <tqchen@users.noreply.github.com>
2025-11-04 12:07:09 -0500

    [LINT] Bump macro version after release (#223)
    This PR bumps macro version after release

--- include/tvm/ffi/c_api.h
+++ include/tvm/ffi/c_api.h
@@ -62,7 +62,7 @@
 /*! \brief TVM FFI minor version. */
 #define TVM_FFI_VERSION_MINOR 1
 /*! \brief TVM FFI patch version. */
-#define TVM_FFI_VERSION_PATCH 1
+#define TVM_FFI_VERSION_PATCH 2
 // NOLINTEND(modernize-macro-to-enum)
 
 #ifdef __cplusplus
